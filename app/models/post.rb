141a374b4761d32017b0efbc31337081be7ccd91
christopher-hague <christopher.a.hague@gmail.com>
2017-04-16 18:20:52 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    # unless post.title.include buzz words...
+    unless post.title =~ /Won't Believe|Secret|Guess|Top \d/
+      # errors[:title] << text
+      post.errors[:title] << "This title isn't clickbait-y enough!"
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
